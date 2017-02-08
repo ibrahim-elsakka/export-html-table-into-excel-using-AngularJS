07f889308134d4bbac48d6053a16b94a01f71697
leoseth <voicecontact@yahoo.com>
2017-02-07 23:26:41 -0500

    adding files

---
+++ export/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="export.MvcApplication" Language="C#" %>
---
+++ export/js/export.js
@@ -0,0 +1,26 @@
+﻿
+var applic = angular.module("myapp", []);
+// SaveAs Function
+applic.controller("exportcontroller", function ($scope, $http) {
+    $scope.fnExcelReport = function () {
+        //applic.controller("exportcontroller", function ($scope, $http) {
+        //   $scope.fnExcelReport = function () {
+        //       var data = new Blob([document.getElementById('dataTableDiv').innerHTML], {
+        //           type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        //       });
+        //       saveAs(data, "Sample.xls");
+        //   }                                                                   
+        //});
+
+        var a = document.createElement('a');
+        //getting data from our div that contains the HTML table
+        var data_type = 'data:application/vnd.ms-excel';
+        var table_div = document.getElementById('dataTableDiv');
+        var table_html = table_div.outerHTML.replace(/ /g, '%20');
+        a.href = data_type + ', ' + table_html;
+        //setting the file name
+        a.download = 'Samples.xls';
+        a.click();
+        //a.preventDefault();
+    }
+});
